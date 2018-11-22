1d1244d126bfb56fffbe507a634814ac4cc24403
Magnarox <thevinc@gmx.fr>
2018-11-22 15:41:28 +0100

    FIX Dashboard Title

--- src/main/webapp/app/analysis/visualisation/visualisation.route.ts
+++ src/main/webapp/app/analysis/visualisation/visualisation.route.ts
@@ -6,6 +6,10 @@ export const visualisationRoute: Routes = [
     {
         path: 'visualisation',
         component: VisualisationComponent,
+        data: {
+            authorities: ['ROLE_USER'],
+            pageTitle: 'analysis.title'
+        },
         canActivate: [UserRouteAccessService]
     }
 ];
