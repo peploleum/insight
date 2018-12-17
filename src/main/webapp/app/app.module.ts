8c3214a2f3a7eda371533131ef9a1ebdf4064b31
peploleum <peploleum@gmail.com>
2018-12-17 15:53:48 +0100

    dashboard front template

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,30 +1,38 @@
 import './vendor.ts';
 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
-import { Ng2Webstorage } from 'ngx-webstorage';
-import { NgJhipsterModule } from 'ng-jhipster';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {NgbDatepickerConfig} from '@ng-bootstrap/ng-bootstrap';
+import {Ng2Webstorage} from 'ngx-webstorage';
+import {NgJhipsterModule} from 'ng-jhipster';
 
-import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
-import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
-import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
-import { InsightSharedModule } from 'app/shared';
-import { InsightCoreModule } from 'app/core';
-import { InsightAppRoutingModule } from './app-routing.module';
-import { InsightHomeModule } from './home/home.module';
-import { InsightAccountModule } from './account/account.module';
-import { InsightEntityModule } from './entities/entity.module';
+import {AuthExpiredInterceptor} from './blocks/interceptor/auth-expired.interceptor';
+import {ErrorHandlerInterceptor} from './blocks/interceptor/errorhandler.interceptor';
+import {NotificationInterceptor} from './blocks/interceptor/notification.interceptor';
+import {InsightSharedModule} from 'app/shared';
+import {InsightCoreModule} from 'app/core';
+import {InsightAppRoutingModule} from './app-routing.module';
+import {InsightHomeModule} from './home/home.module';
+import {InsightAccountModule} from './account/account.module';
+import {InsightEntityModule} from './entities/entity.module';
 import * as moment from 'moment';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
-import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent, ActiveMenuDirective, ErrorComponent } from './layouts';
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+import {DashboardModule} from 'app/dashboard/dashboard.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         InsightAppRoutingModule,
-        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-' }),
+        Ng2Webstorage.forRoot({prefix: 'jhi', separator: '-'}),
         NgJhipsterModule.forRoot({
             // set below to true to make alerts look like toast
             alertAsToast: false,
@@ -37,7 +45,8 @@ import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent
         InsightHomeModule,
         InsightAccountModule,
         // jhipster-needle-angular-add-module JHipster will add new module here
-        InsightEntityModule
+        InsightEntityModule,
+        DashboardModule
     ],
     declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
     providers: [
@@ -61,6 +70,6 @@ import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent
 })
 export class InsightAppModule {
     constructor(private dpConfig: NgbDatepickerConfig) {
-        this.dpConfig.minDate = { year: moment().year() - 100, month: 1, day: 1 };
+        this.dpConfig.minDate = {year: moment().year() - 100, month: 1, day: 1};
     }
 }
