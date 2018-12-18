96e591eebf5180c7e436ffe39c732adae7101b6b
peploleum <peploleum@gmail.com>
2018-12-18 18:36:50 +0100

    added mongodb local storage PV and PVC

--- src/main/docker/kubernetes/new/volumes/create_volumes.sh
+++ src/main/docker/kubernetes/new/volumes/create_volumes.sh
@@ -4,3 +4,4 @@ mkdir -p /tmp/kubernetes/INSIGHT/master
 mkdir -p /tmp/kubernetes/INSIGHT/data
 mkdir -p /tmp/kubernetes/INSIGHT/logstash/
 cp ./logstash/* /tmp/kubernetes/INSIGHT/logstash/
+mkdir -p /tmp/kubernetes/INSIGHT/mongodb/
