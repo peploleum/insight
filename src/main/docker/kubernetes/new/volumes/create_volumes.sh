416c6dcdcf66a63a15e1f2eaa2d3218f30a73f54
peploleum <peploleum@gmail.com>
2018-12-18 16:12:04 +0100

    kubernetes setup update

--- src/main/docker/kubernetes/new/volumes/create_volumes.sh
+++ src/main/docker/kubernetes/new/volumes/create_volumes.sh
@@ -1,4 +1,6 @@
 mkdir /tmp/kubernetes/INSIGHT/nifi/
+cp ./nifi/* /tmp/kubernetes/INSIGHT/nifi/
 mkdir /tmp/kubernetes/INSIGHT/master
 mkdir /tmp/kubernetes/INSIGHT/data
 mkdir /tmp/kubernetes/INSIGHT/logstash/
+cp ./logstash/* /tmp/kubernetes/INSIGHT/logstash/
