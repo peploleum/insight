ddea6e1d2ca2b09064407d53a1166d5ba9e9f880
peploleum <peploleum@gmail.com>
2018-12-18 11:58:27 +0100

    kubernetes setup

---
+++ src/main/docker/kubernetes/old/kubectl-apply.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+kubectl apply -f namespace.yml
+kubectl apply -f volumes/
+kubectl apply -f insight/
+kubectl apply -f messagebroker/
+kubectl apply -f console/
