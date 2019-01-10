e39e2b86adc4f5c76b69762ab097c519f0ea6a4a
peploleum <peploleum@gmail.com>
2019-01-10 20:10:47 +0100

    css hotfix

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { errorRoute, navbarRoute } from './layouts';
 import { DEBUG_INFO_ENABLED } from 'app/app.constants';
-import { sideRoute } from 'app/map/side.route';
 
-const LAYOUT_ROUTES = [navbarRoute, sideRoute, ...errorRoute];
+const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
 
 @NgModule({
     imports: [
--- src/main/webapp/app/map/side.route.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-
-import { EventThreadComponent } from 'app/map/event-thread.component';
-
-export const sideRoute: Route = {
-    path: 'map',
-    component: EventThreadComponent,
-    outlet: 'side'
-};
