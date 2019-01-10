ad8ab82ab4cf324b31bfbace2647e240ae44308a
peploleum <peploleum@gmail.com>
2019-01-10 19:23:33 +0100

    dirty hotfix

--- src/main/webapp/app/dashboard/dashboard.service.ts
+++ src/main/webapp/app/dashboard/dashboard.service.ts
@@ -1,12 +1,12 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {SERVER_API_URL} from 'app/app.constants';
-import {IEntityMappingInfo, IKibanaDashboardGenerationParameters} from './kibana-object.model';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DEBUG_INFO_ENABLED, SERVER_API_URL } from 'app/app.constants';
+import { IEntityMappingInfo, IKibanaDashboardGenerationParameters } from './kibana-object.model';
 
 type EntityResponseType = HttpResponse<string[]>;
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DashboardService {
     private resourceUrl = SERVER_API_URL + '/api/elastic-client-resource';
     private _getDashboardId = '/get-dashboard-ids';
@@ -18,23 +18,27 @@ export class DashboardService {
     kibanaUrl: string;
 
     constructor(private http: HttpClient) {
-        this.kibanaUrl = 'http://' + window.location.hostname + ':5601/';
+        if (DEBUG_INFO_ENABLED) {
+            this.kibanaUrl = 'http://' + window.location.hostname + ':5601/';
+        } else {
+            this.kibanaUrl = 'http://10.65.34.149:30100';
+        }
     }
 
     getEntitiesSchema(): Observable<HttpResponse<IEntityMappingInfo[]>> {
-        return this.http.get<IEntityMappingInfo[]>(`${this.resourceUrl + this._getEntitiesSchema}`, {observe: 'response'});
+        return this.http.get<IEntityMappingInfo[]>(`${this.resourceUrl + this._getEntitiesSchema}`, { observe: 'response' });
     }
 
     getDashboardIds(): Observable<EntityResponseType> {
-        return this.http.get<string[]>(`${this.resourceUrl + this._getDashboardId}`, {observe: 'response'});
+        return this.http.get<string[]>(`${this.resourceUrl + this._getDashboardId}`, { observe: 'response' });
     }
 
     regenerateDashboard(): Observable<HttpResponse<any>> {
-        return this.http.get<any>(`${this.resourceUrl + this._regenerateDashboard}`, {observe: 'response'});
+        return this.http.get<any>(`${this.resourceUrl + this._regenerateDashboard}`, { observe: 'response' });
     }
 
     deleteAllDashboard(): Observable<HttpResponse<any>> {
-        return this.http.delete<any>(`${this.resourceUrl + this._deleteAllDashboard}`, {observe: 'response'});
+        return this.http.delete<any>(`${this.resourceUrl + this._deleteAllDashboard}`, { observe: 'response' });
     }
 
     /**
@@ -42,6 +46,6 @@ export class DashboardService {
      * @Return Observable<EntityResponseType> contenant String[], l'id du dashboard créé
      * */
     postDashboard(dashboardParam: IKibanaDashboardGenerationParameters): Observable<EntityResponseType> {
-        return this.http.post<string[]>(this.resourceUrl + this._postDashboard, dashboardParam, {observe: 'response'});
+        return this.http.post<string[]>(this.resourceUrl + this._postDashboard, dashboardParam, { observe: 'response' });
     }
 }
