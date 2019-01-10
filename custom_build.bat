203cb32b42757bfba45a043ceefbbe42093ab69f
peploleum <peploleum@gmail.com>
2019-01-10 20:41:02 +0100

    fixed build script with version

--- custom_build.bat
+++ custom_build.bat
@@ -1,4 +1,4 @@
 mvn package -DskipTests -Pprod verify jib:dockerBuild
 docker login --username=peploleum
-docker tag insight peploleum/insight:latest
-docker push peploleum/insight:latest
+docker tag insight peploleum/insight:1.0.0
+docker push peploleum/insight:1.0.0
