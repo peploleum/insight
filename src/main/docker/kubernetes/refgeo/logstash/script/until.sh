949354a5567bf6346182278fcb84eed6518db39b
grjamet <gregory.jamet@capgemini.com>
2019-01-21 18:31:17 +0100

    logstash for full gazetteer

--- src/main/docker/kubernetes/refgeo/logstash/script/until.sh
+++ src/main/docker/kubernetes/refgeo/logstash/script/until.sh
@@ -9,19 +9,22 @@ do
 done
 curl  -X PUT 'refgeo-elasticsearch:9200/gazetter?pretty' -H 'Content-Type: application/json' -d '{
   "settings" : {
-    "number_of_shards" : 1
-  },
-  "mappings" : {
-    "doc" : {
-      "properties" : {
-        "asciiname" : { "type" : "text" },
-        "countrycode" : { "type" : "text" },
-        "latitude" : { "type" : "text" },
-        "longitude" : { "type" : "text" },
-        "name" : { "type" : "text" },
-        "population" : { "type" : "text" },
-        "location" : { "type" : "geo_point"}
-      }
+        "number_of_shards" : 1
+    },
+    "mappings" : {
+        "doc" : {
+            "properties" : {
+                "geonameid" : { "type" : "text" },
+				"sciiname" : { "type" : "text" },
+				"countrycode" : { "type" : "text" },
+				"fclass" : { "type" : "text" },
+				"fcode" : { "type" : "text" },
+				"latitude" : { "type" : "text" },
+				"longitude" : { "type" : "text" },
+				"name" : { "type" : "text" },
+				"population" : { "type" : "text" },
+				"location" : { "type" : "geo_point"}
+            }
+        }
     }
-  }
 }'
