ecbaed3fb227667f4bdb7a326c4632f18b1bbd78
Lothrec <rasta.populos14@gmail.com>
2019-01-31 18:36:48 +0100

    modif startup.groovy mixedIndex

--- src/main/docker/kubernetes/graph/conf/startup.groovy
+++ src/main/docker/kubernetes/graph/conf/startup.groovy
@@ -14,10 +14,14 @@ globals << [hook: [
             propertyKey = mgmt.getPropertyKey('idInsight')
             if (propertyKey == null) {
                 propertyKey = mgmt.makePropertyKey('idInsight').dataType(String.class).make()
+                mgmt.makeVertexLabel("idInsight").make()
             }
             ctx.logger.warn("property key  : " + propertyKey.toString())
-            compositeIndex = mgmt.buildIndex('insightIdNameComposite', Vertex.class).addKey(propertyKey).buildCompositeIndex()
-            ctx.logger.warn("compositeIndex  : " + compositeIndex.toString())
+            //compositeIndex = mgmt.buildIndex('insightIdNameComposite', Vertex.class).addKey(propertyKey).buildCompositeIndex()
+            //ctx.logger.warn("compositeIndex  : " + compositeIndex.toString())
+            
+            mixedIndex = mgmt.buildIndex('insightIdNameMixed', Vertex.class).addKey(propertyKey).buildMixedIndex("search")
+            ctx.logger.warn("compositeIndex  : " + mixedIndex.toString())
             mgmt.commit()
             res = graph.openManagement().containsGraphIndex('insightIdNameComposite')
             ctx.logger.warn("Index created : " + res.toString())
