bd1e4db29ad7b96921a168a391be21492f1ed545
Got29 <gafolgoa@saer.euriware>
2019-02-12 19:14:10 +0100

    Integration Chart.js

--- src/main/webapp/app/map/side/quick-stats.component.ts
+++ src/main/webapp/app/map/side/quick-stats.component.ts
@@ -1,20 +1,28 @@
 /**
  * Created by gFolgoas on 31/01/2019.
  */
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { SideInterface } from '../../shared/side/side.abstract';
 import { MapService } from '../map.service';
+import * as Chart from 'chart.js';
 
 @Component({
     selector: 'ins-quick-stats',
     templateUrl: './quick-stats.component.html'
 })
-export class QuickStatsComponent extends SideInterface implements OnInit, OnDestroy {
+export class QuickStatsComponent extends SideInterface implements OnInit, AfterViewInit, OnDestroy {
+    chart: Chart;
+    @ViewChild('canvas') canvas: ElementRef;
+
     constructor(protected ms: MapService) {
         super();
     }
 
     ngOnInit() {}
+
+    ngAfterViewInit() {
+        this.chart = new Chart((<HTMLCanvasElement>this.canvas.nativeElement).getContext('2d'), {});
+    }
 
     ngOnDestroy() {}
 }
