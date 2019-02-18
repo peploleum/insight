860617ccf128ba6dfa37b37ed0ea414f4ae3f2de
peploleum <peploleum@gmail.com>
2019-02-18 13:32:01 +0100

    refgeo : Modif volume location

--- src/main/docker/kubernetes/refgeo/volume/volumes.sh
+++ src/main/docker/kubernetes/refgeo/volume/volumes.sh
@@ -1,7 +1,7 @@
-mkdir -p /home/capgemini/binding/refgeo/logstash/
-mkdir -p /home/capgemini/binding/refgeo/logstash/csv
-cp ../logstash/csv/* /home/capgemini/binding/refgeo/logstash/csv
-mkdir -p /home/capgemini/binding/refgeo/logstash/pipeline
-cp ../logstash/pipeline/* /home/capgemini/binding/refgeo/logstash/pipeline
-mkdir -p /home/capgemini/binding/refgeo/logstash/script
-cp ../logstash/script/* /home/capgemini/binding/refgeo/logstash/script
+mkdir -p /home/capgemini/binding/INSIGHT/refgeo/logstash/
+mkdir -p /home/capgemini/binding/INSIGHT/refgeo/logstash/csv
+cp ../logstash/csv/* /home/capgemini/binding/INSIGHT/refgeo/logstash/csv
+mkdir -p /home/capgemini/binding/INSIGHT/refgeo/logstash/pipeline
+cp ../logstash/pipeline/* /home/capgemini/binding/INSIGHT/refgeo/logstash/pipeline
+mkdir -p /home/capgemini/binding/INSIGHT/refgeo/logstash/script
+cp ../logstash/script/* /home/capgemini/binding/INSIGHT/refgeo/logstash/script
