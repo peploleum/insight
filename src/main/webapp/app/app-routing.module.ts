186222b983d2040090395328de42063ed8a8b8aa
Got29 <gafolgoa@saer.euriware>
2019-02-25 13:29:17 +0100

    Modification router enablingDebug false

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
                     loadChildren: './admin/admin.module#InsightAdminModule'
                 }
             ],
-            { useHash: true, enableTracing: true }
+            { useHash: true, enableTracing: false }
         )
     ],
     exports: [RouterModule]
