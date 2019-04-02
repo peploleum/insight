a44047152290ce5dd47e34a30e733e9926cb7721
peploleum <peploleum@gmail.com>
2019-04-02 12:30:02 +0200

    fixed init.sh indent

--- src/main/docker/compose/graph/janus/init.sh
+++ src/main/docker/compose/graph/janus/init.sh
@@ -4,7 +4,8 @@ while true
 do
     rt=$(nc -z -w 1 cassandra 9042)
     if [ $? -eq 0 ]; then
-    echo "CASSANDRA is UP" break
+        echo "CASSANDRA is UP"
+        break
     fi
     echo "CASSANDRA is not yet reachable;sleep for 10s before retry" sleep 10
 done
