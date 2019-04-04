11d9b1b8383eb29056dd23151564ed600007314a
Got29 <gf29.work@gmail.com>
2019-04-04 10:39:16 +0200

    Ajout condition sur profil sur les restControllers

--- src/main/java/com/peploleum/insight/web/rest/GraphResource.java
+++ src/main/java/com/peploleum/insight/web/rest/GraphResource.java
@@ -6,6 +6,7 @@ import com.peploleum.insight.web.rest.util.HeaderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ import java.util.LinkedHashMap;
  */
 @RestController
 @RequestMapping("/api/graph")
-@ConditionalOnExpression("#{environment.getProperty('spring.profiles.active').contains('graphy')}")
+@Profile("graphy")
 public class GraphResource {
     private final Logger log = LoggerFactory.getLogger(GraphResource.class);
     private final InsightGraphRelationService insightGraphRelationService;
