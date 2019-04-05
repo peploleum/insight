345641592f1d71e98ff437c7737eff91d44f9236
peploleum <peploleum@gmail.com>
2019-04-05 18:34:56 +0200

    added standalone insight deployment fixed uber yml sleep paramters

--- custom_build.bat
+++ custom_build.bat
@@ -1,4 +1,4 @@
-call mvn package -DskipTests -Pprod verify jib:dockerBuild
+call mvn package -DskipTests -Pprod -Pgraphy verify jib:dockerBuild
 docker login --username=peploleum
-docker tag insight peploleum/insight:1.0.0
-docker push peploleum/insight:1.0.0
+docker tag insight peploleum/insight:latest
+REM docker push peploleum/insight:1.0.0
